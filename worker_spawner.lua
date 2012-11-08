66c50a8b06e1e56e5b070edbb2757decd60f54b9
Thomas R. Koll <tomk32@tomk32.de>
2012-11-08 18:50:03 +0100

    Give proper random names to workers

--- worker_spawner.lua
+++ worker_spawner.lua
@@ -1,12 +1,18 @@
 
 class "WorkerSpawner" {
 
-  names = {'A','B','C'},
+  first_names = {'Amot','Bitan','Calot', 'Difnatan'},
+  last_names = {'Zok', 'Yulkomnek', 'Xixik', 'Weikok'},
 
   __init__ = function(self)
   end,
 
   create = function(self)
-    return Worker(self.names[math.random(1,#self.names)], {x = 1, y = math.random(10), z = 1})
+    return Worker(self:random_name(), {x = math.random(14), y = math.random(10), z = 1})
+  end,
+
+  random_name = function(self)
+    return (self.first_names[math.random(#self.first_names)] .. ' ' ..
+      self.last_names[math.random(#self.last_names)] )
   end
 }
