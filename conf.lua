5b1a1bf4bcaecdfee76543669215b519cda73584
Thomas R. Koll <tomk32@tomk32.de>
2012-12-02 00:05:41 +0100

    Add version to window title

--- conf.lua
+++ conf.lua
@@ -1,5 +1,6 @@
+require('version')
 function love.conf(c)
-  c.title = "Cross Country Running"
+  c.title = "Cross Country Running - " .. game.version
   c.author = "Thomas R. Koll"
   c.modules.physics = false
   c.screen.width = 1024
